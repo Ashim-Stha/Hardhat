293f5e4a33a3d2aa9cc5c6e3ecc400043a2a63eb
Ashim <ashimshrestha2384@gmail.com>
2023-10-15 15:48:32 +0545

    sepoliaAlchemy

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,17 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const url = process.env.RPC_URL_ALCHEMY;
+const pkey = process.env.PRIVATE_KEY;
+
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: url,
+      accounts: [pkey],
+      chainId: 11155111,
+    },
+  },
   solidity: "0.8.8",
 };
