26bb38196bf5ad37a8febdf873cddda4cf05e611
Ashim <ashimshrestha2384@gmail.com>
2023-10-15 22:20:32 +0545

    verifyProgramatically

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require("@nomicfoundation/hardhat-verify");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 const url = process.env.RPC_URL_ALCHEMY;
 const pkey = process.env.PRIVATE_KEY;
+const api = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -15,4 +17,7 @@ module.exports = {
     },
   },
   solidity: "0.8.8",
+  etherscan: {
+    apiKey: api,
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,8 @@
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require("hardhat");
+//run allows to run any hardhat tasks
+//network to see network ie testnet or EVM or mainnet
+
+require("dotenv").config();
 
 async function main() {
   const contractFactory = await ethers.getContractFactory("SimpleStorage");
@@ -8,6 +12,29 @@ async function main() {
   const address = await contract.getAddress();
 
   console.log(address);
+  // console.log(network.config);
+
+  if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    await contract.getAddress();
+    await verify(address, []);
+  }
+
+  async function verify(contractAddress, args) {
+    //args is present only if constuctor is present in contract
+    console.log("Verifying contract...");
+    try {
+      await run("verify:verify", {
+        address: contractAddress,
+        constructorArguments: args,
+      });
+    } catch (err) {
+      if (err.message.toLowerCase().includes("already verified")) {
+        console.log("Already verified");
+      } else {
+        console.log(err);
+      }
+    }
+  }
 }
 
 main()
