c8472bfcdbc728150b49e12e577b089f746154c9
Ashim <ashimshrestha2384@gmail.com>
2023-10-18 16:20:07 +0545

    test

---
+++ test/test-deploy.js
@@ -0,0 +1,36 @@
+const { assert } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("SimpleStorage", function () {
+  let SimpleStorageFactory, simpleStorage;
+  beforeEach(async function () {
+    SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+    simpleStorage = await SimpleStorageFactory.deploy();
+  });
+
+  it("Should start with a fav num 0", async function () {
+    const currentVal = await simpleStorage.retrieve();
+    const expectedVal = "0";
+
+    assert.equal(currentVal.toString(), expectedVal);
+  });
+
+  it("Should update when we call store", async function () {
+    const expectedVal = "7";
+    const transactionResponse = await simpleStorage.store(expectedVal);
+    await transactionResponse.wait(1);
+
+    const updatedVal = await simpleStorage.retrieve();
+    assert(updatedVal.toString(), expectedVal);
+  });
+
+  //to run specific test only
+  // it.only("Should update when we call store", async function () {
+  //   const expectedVal = "7";
+  //   const transactionResponse = await simpleStorage.store(expectedVal);
+  //   await transactionResponse.wait(1);
+
+  //   const updatedVal = await simpleStorage.retrieve();
+  //   assert(updatedVal.toString(), expectedVal);
+  // });
+});
